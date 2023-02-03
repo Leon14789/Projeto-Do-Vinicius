30d1b524cbd3ec7084eb95833903cf7e3155c1cb
Leonardo Ortiz <leoortizinho23@gmail.com>
2023-02-03 15:03:58 -0300

    Subindo o Site e colocando o botao login

---
+++ resources/views/index.blade.php
@@ -0,0 +1,146 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<title>AUTO SOCORRO XAVIER</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<link rel="stylesheet" href="assets/css/main.css" />
+	<link rel="stylesheet" href="assets/css/responsividade.css" />
+	<link rel="stylesheet" href="assets/css/bootstrap.css" />
+	<link rel="stylesheet" href="assets/css/fundo-banner.css" />
+	<noscript>
+		<link rel="stylesheet" href="assets/css/noscript.css" />
+	</noscript>
+</head>
+
+<body class="is-preload landing">
+	<div id="page-wrapper">
+
+		<!-- Header -->
+		<header id="header">
+
+			<nav id="nav">
+				<button type="button" class="btn btn-primary" href="pao">
+					<a class="acss-sistem" href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">ACESSAR SISTEMA</a>
+				</button>
+
+			</nav>
+		</header>
+
+		<!-- Banner -->
+		<section id="banner" class="header-fundo">
+			<div class="content">
+
+
+
+
+				<img class="img-logo" src="assets/images/logo.png" alt="logo-empresa" />
+			</div>
+			<a href="#one" class="goto-next scrolly">Next</a>
+		</section>
+
+		<!-- One -->
+		<section id="one" class="spotlight style1 bottom">
+			<span class="image fit main"><img src="assets/images/pic02.png" alt="" /></span>
+			<div class="content">
+				<div>
+					<div class="row">
+						<div class="col-12 col-12-medium">
+							<header>
+								<h2 class="txt">A empresa Auto Socorro Xavier</h2>
+								<p class="txt">Te Deseja Boas Vindas ao nosso SISTEMA</p>
+							</header>
+						</div>
+
+					</div>
+				</div>
+			</div>
+			<a href="#two" class="goto-next scrolly">Next</a>
+		</section>
+
+		<!-- Two -->
+		<section id="two" class="spotlight style2 right">
+			<span class="image fit main"><img src="assets/images/quem-somos.png" alt="" /></span>
+			<div class="content" id="flex-main">
+				<header>
+					<h2 class="txt">QUEM SOMOS?</h2>
+				</header>
+				<p>Somos uma empresa prestadora de guincho,na qual trabalhamos com um alto padrão de atendimento aos
+					nossos clientes. somos uma das Prestadora de guincho mais tradicionais da cidade de São Paulo.
+					<br> Atualmente estamos localizados (ENDERECO) mas realizamos atendimento em toda região de SP
+				</p>
+
+
+
+				<!-- Button trigger modal -->
+				<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
+					Acompanhe um pouco da nossa empresa
+				</button>
+
+			</div>
+			<!-- Modal -->
+			<div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog"
+				aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+				<div class="modal-dialog" role="document">
+					<div class="modal-content">
+						<div class="modal-header">
+							<h5 class="modal-title" id="exampleModalLongTitle">UM POUCO SOBRE NOSSA EMPRESA</h5>
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+								<span aria-hidden="true">&times;</span>
+							</button>
+						</div>
+						<div class="modal-body">
+							<video class="video" src="assets/images/video.mp4" controls ></video>
+						</div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+			<a href="#three" class="goto-next scrolly">Next</a>
+		</section>
+
+		<!-- Three -->
+		<section id="three" class="spotlight style3 left">
+			<div>
+				<span class="image fit main bottom"><img src="assets/images/pic04.png" alt="" /></span>
+			</div>
+
+			<div class="content" id="flex-main">
+				<header>
+					<h2 class="txt">PARCEIROS</h2>
+				</header>
+				<p>Analizadno a foto anexada ao lado você podera verificar alguma das empresas parceiras que trabalhamos
+					atualmente</p>
+			</div>
+		</section>
+
+
+
+		<!-- Footer -->
+		<footer id="footer">
+			<ul class="copyright">
+				<li>&copy; AUTO SOCORRO XAVIER.</li>
+				<li>Design: Leonardo O. Alves</li>
+			</ul>
+		</footer>
+
+	</div>
+
+	<!-- Scripts -->
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/jquery.scrolly.min.js"></script>
+	<script src="assets/js/jquery.dropotron.min.js"></script>
+	<script src="assets/js/jquery.scrollex.min.js"></script>
+	<script src="assets/js/browser.min.js"></script>
+	<script src="assets/js/breakpoints.min.js"></script>
+	<script src="assets/js/util.js"></script>
+	<script src="assets/js/main.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
